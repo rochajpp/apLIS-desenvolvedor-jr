b81d6893daf73ff6f8340d8875db91a223e57142
João Paulo Rocha <joao.rochap03@gmail.com>
2026-04-16 21:32:23 -0300

    :sparkles: feat: Adiciona lógica de inserção de paciente

--- backendjs/src/controllers/pacientes.js
+++ backendjs/src/controllers/pacientes.js
@@ -4,14 +4,24 @@ module.exports.getAll = async (app, req, res) => {
         const Pacientes = new app.src.models.Pacientes(context);
         const response = await Pacientes.getAll();
         res.status(200).json(response);
+        return;
     } catch(err) {
         res.status(500).json(err);
         return;
     }
 }
 
-module.exports.add = (app, req, res) => {
-    res.send("Adicionando novo paciente");
-    return;
+module.exports.add = async (app, req, res) => {
+    try{
+        const data = req.body;
+        const context = app.config.db_config;
+        const Pacientes = new app.src.models.Pacientes(context);
+        const response = await Pacientes.add(data);
+        res.status(200).json(response);
+        return;
+    } catch(err){
+        res.status(500).json(err);
+        return;
+    }
 }
 
--- backendjs/src/models/Pacientes.js
+++ backendjs/src/models/Pacientes.js
@@ -14,6 +14,18 @@ class Pacientes{
             throw err;
         }
     }
+
+
+    async add(paciente){
+        try{
+            const query = `INSERT INTO pacientes (Id, Nome, DataNascimento, Carteirinha, CPF) VALUES (${paciente.id}, '${paciente.nome}', '${paciente.dataNascimento}', '${paciente.carteirinha}', '${paciente.cpf}')`;
+            const [rows] = await this.context.query(query);
+            return rows;
+        } catch(err){
+            console.error(err);
+            throw err;
+        }
+    }
 }
 
 module.exports = () => {
