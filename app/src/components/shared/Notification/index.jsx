91c062059d3d39cf12b97e80f9f18afddd010685
João Paulo Rocha <joao.rochap03@gmail.com>
2026-04-21 18:46:10 -0300

    :sparkles: feat: Atualiza componente Notification e resposta de backendjs

--- app/src/components/shared/Notification/index.jsx
+++ app/src/components/shared/Notification/index.jsx
@@ -1,24 +1,40 @@
+import { useEffect, useState } from 'react'
+
 import styles from "./styles.module.css"
 
 function Notification({ title, message, type }) {
+
+    const [open, setOpen] = useState(false);
+
+    useEffect(() => {
+        const main = document.querySelector(`.${styles.main}`);
+
+        main.style.right = "0";
+
+        setTimeout(() => {
+            main.style.right = "-100%";
+        }, 5000)
+    }, [])
+
+
     if (type == 1) {
         return (
-            <div className={styles.main}>
-                <div className={styles.title}>
-                    <ion-icon name="checkmark-circle"></ion-icon>
+            <div className={`${styles.main} ${styles.type1}`}>
+                <ion-icon className={styles.iconMain} name="checkmark-circle"></ion-icon>
+                <div className={styles.message}>
                     <h2>{title}</h2>
+                    {message}
                 </div>
-                {message}
             </div>
         )
-    } else if(type == 2){
-         return (
-            <div className={styles.main}>
-                <div className={styles.title}>
-                    <ion-icon name="close-circle"></ion-icon>
+    } else if (type == 2) {
+        return (
+            <div className={`${styles.main} ${styles.type2}`}>
+                <ion-icon className={styles.iconMain} name="close-circle"></ion-icon>
+                <div className={styles.message}>
                     <h2>{title}</h2>
+                    {message}
                 </div>
-                {message}
             </div>
         )
     }
--- backendjs/src/controllers/patients.js
+++ backendjs/src/controllers/patients.js
@@ -6,7 +6,10 @@ module.exports.getAll = async (app, req, res) => {
         res.status(200).json(response);
         return;
     } catch(err) {
-        res.status(500).json(err);
+        res.status(500).json({
+            msg: "Houve um erro ao obter pacientes",
+            err: err
+        });
         return;
     }
 }
@@ -23,7 +26,10 @@ module.exports.add = async (app, req, res) => {
         });
         return;
     } catch(err){
-        res.status(500).json(err);
+        res.status(500).json({
+            msg: "Houve um erro ao cadastra paciente",
+            err: err
+        });
         return;
     }
 }
